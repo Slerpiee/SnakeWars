6941ba896c8271bec0f16b5baf70efb5348b460f
Slerpiee <leonid.d.polyakov@gmail.com>
2025-10-15 21:49:21 +0300

    merget

--- Game/internal/game/models/player.go
+++ Game/internal/game/models/player.go
@@ -30,8 +30,8 @@ type Segment struct {
 
 const MIN_SEGMENT_LEN = 5.0 //Минимальная длина змейки 
 
-type User struct{
-	ID string `json:"id"`
+type User struct {
+	ID       string `json:"id"`
 	Username string `json:"username"`
 	Room_id  string
 }
@@ -58,15 +58,74 @@ type Snake struct {
 	Speed Speed
 	Skin  PlayerCosmetics
 	State PlayerState
-	Body []Segment //очередь
+	Stats PlayerStats
+	Body  []Segment //очередь
+	SegmentCounter int
 }
 
-func (s *Snake) Move(p Point){
-	
-	if (len(s.Body) == 0){
+func (s *Snake) Add_Segment(seg Segment) int{
+	s.Body = append(s.Body, seg)
+	s.SegmentCounter += 1
+	return s.SegmentCounter
+}
+
+func (s *Snake) Remove_Segment(seg Segment) int{
+	s.SegmentCounter > 0
+}
+
+//База:
+func NewSnake(id string, startPoint Point, color string) *Snake {
+	StartBody := []Segment{
+		{
+			Start:     startPoint,
+			End:       startPoint,
+			Length:    0,
+			Direction: 0,
+		},
+	}
+	return &Snake{
+		ID:    id,
+		Head:  startPoint,
+		Speed: Speed{Dx: 1, Dy: 0},
+		Skin:  PlayerCosmetics{Color: color},
+		State: PlayerState{isAlive: true, isReady: false},
+		Stats: PlayerStats{
+			Wins:        0,
+			Losses:      0,
+			GamesPlayed: 0,
+			MaxLength:   0,
+		},
+		Body: StartBody,
+	}
+}
+
+func (s *Snake) Grow(amount float64){
+	//добавление в кольцевой буффер
+}
+
+func (s *Snake) GetHead() Point{
+	point := Point{s.Head.X, s.Head.Y}
+	return point
+}
+
+
+//Движение:
+func (s *Snake) Move(p Point) {
+
+	if len(s.Body) == 0 {
 		s.Body = append(s.Body, Segment{s.Head, p, distance(s.Head, p), direction(s.Head, p)})
-	} else{
-		//dist := s.Body[len(s.Body)-1]
-		
+		s.SegmentCounter = 1
+	} else {
+		head_segment := &s.Body[len(s.Body)-1]
+		dist := distance(head_segment.End, p)
+		new_direction := direction(head_segment.End, p)
+		if math.Abs(head_segment.Direction - new_direction) < eps_rad {
+			head_segment.End = p
+			head_segment.Length += dist
+		} else{
+			new_seg := Segment{s.Head, p, dist, new_direction}
+			
+		}
+
 	}
 }

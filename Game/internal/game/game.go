442da214ba96e0117b2702366809088a66b5b9a5
Slerpiee <leonid.d.polyakov@gmail.com>
2025-10-28 21:20:20 +0300

    user

--- Game/internal/game/game.go
+++ Game/internal/game/game.go
@@ -11,6 +11,7 @@ type RoomState struct{}
 
 type RoomStats struct{}
 
+
 type RoomMessage struct {
     Message  any //Дописать структуру для сообщений
 }
@@ -18,17 +19,13 @@ type RoomMessage struct {
 
 type Room struct{
 	ID string //Room ID
-
-	
 	Users sync.Map  //ID: *User
-
 
 	room_mutex sync.RWMutex
 	State RoomState
 	Stats RoomStats
 
 	roomInput chan RoomMessage
-	//RoomChan chan
 	ticker *time.Ticker //update per second 
 
 }
@@ -52,7 +49,6 @@ func (room *Room) getUser(id string) *User {
 }
 
 
-
 func (room *Room) UpdateUser(id string) {
     if User := room.getUser(id); User != nil {
         User.mutex.Lock() 
@@ -60,6 +56,7 @@ func (room *Room) UpdateUser(id string) {
 		User.Snake.Move(time.Since(User.LastPing), false)
     }
 }
+
 
 func (room *Room) Broadcast(message interface{}) {
     room.Users.Range(func(key, value interface{}) bool {
--- Game/internal/game/user.go
+++ Game/internal/game/user.go
@@ -6,12 +6,27 @@ import (
 	"sync"
 )
 
+type UserState int
+
+const (
+    UserStateNotReady UserState = 0
+    UserStateReady UserState = 1
+    UserStateKilled UserState = 2
+	UserStateExit UserState = 3
+)
+
+
 type User struct {
 	ID    string
 	Conn  *websocket.Conn
 	Snake *Snake
+	State UserState
 	SendChan  chan any //Канал для отправки сообщений клиенту
 	mutex    sync.RWMutex
     LastPing time.Time
 }
+
+
+
+
 
